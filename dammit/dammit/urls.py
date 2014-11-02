2a2d3913e71f2fe8ca6320004ec08ea32c3945dd
Michiel Scholten <michiel@aquariusoft.org>
2014-11-02 16:34:33 +0100

    Started stubbing

--- dammit/dammit/urls.py
+++ dammit/dammit/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'dammit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'weblog.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- dammit/weblog/admin.py
+++ dammit/weblog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.db import models
+from .models import (
+        KontentUser,
+        Site,
+        Tag,
+        ContentGroup,
+        ContentItem)
 
-# Register your models here.
+
+class KontentUserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    search_fields = ['name', 'user']
+
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ['title']
+
+
+class ContentGroupAdmin(admin.ModelAdmin):
+    #list_display = ('name', 'parent')
+    #list_display = ('site')
+    list_display = ('title',)
+
+
+class ContentItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published', 'public', 'publish_from', 'publish_to')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag', 'slug')
+
+
+admin.site.register(KontentUser, KontentUserAdmin)
+admin.site.register(Site, SiteAdmin)
+admin.site.register(ContentGroup, ContentGroupAdmin)
+admin.site.register(ContentItem, ContentItemAdmin)
+admin.site.register(Tag, TagAdmin)
