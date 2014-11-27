7fba32234a53ea0bb2c0ee044066eb05c3d73e13
Michiel Scholten <michiel@aquariusoft.org>
2014-11-27 10:28:21 +0100

    Added automated admin docs

--- dammit/dammit/urls.py
+++ dammit/dammit/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', include('weblog.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
