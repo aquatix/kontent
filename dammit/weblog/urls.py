4f26740faf73a0e144442eab76b5b0a9fd2d1acc
Michiel Scholten <michiel@aquariusoft.org>
2014-12-08 07:17:39 +0100

    Some stubbing

--- dammit/weblog/urls.py
+++ dammit/weblog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from weblog import views
+from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^/p/([0-9]{4})/$', views.post, name='post-detail'),
+    url(r'^/archive/([0-9]{4})/$', views.archive, name='archive-year'),
 ]
