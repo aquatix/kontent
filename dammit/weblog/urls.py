71af82f10ec59ac67b402e10efd5f70df61136ed
Michiel Scholten <michiel@aquariusoft.org>
2014-12-09 16:41:38 +0100

    Get an article

--- dammit/weblog/urls.py
+++ dammit/weblog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^p/(?P<postid>[0-9]+)$', views.post, name='post-detail'),
+    url(r'^p/(?P<article_id>[0-9]+)$', views.article, name='article-detail'),
     url(r'^archive/(?P<year>[0-9]{4})/$', views.archive, name='archive-year'),
     url(r'^archive/$', views.archive, name='archive-year'),
 ]
--- dammit/weblog/views.py
+++ dammit/weblog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import *
 
 def home(request):
     return HttpResponse('You are home. Have a hot beverage.')
 
 
-def post(request, postid):
-    return HttpResponse('You are viewing a post.')
+def article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'article.html', {'article': article})
 
 
 def archive(request, year=None):
-    return HttpResponse('You are viewing the archive.')
+    return HttpResponse('You are viewing the archive (year: {0}).'.format(year))
