a7f6a9a8953ccaa5ef77c7cbf234964e28152ab8
Michiel Scholten <michiel@aquariusoft.org>
2014-12-29 09:04:23 +0100

    Some fixes, new initial migrations

---
+++ dammit/weblog/migrations/0001_initial.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import autoslug.fields
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sites', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('date_modified', models.DateTimeField(auto_now=True, null=True)),
+                ('title', models.CharField(max_length=255, blank=True)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False)),
+                ('public', models.BooleanField(default=False)),
+                ('published', models.BooleanField(default=False)),
+                ('publish_from', models.DateTimeField(null=True, blank=True)),
+                ('publish_to', models.DateTimeField(null=True, blank=True)),
+                ('protected', models.BooleanField(default=False)),
+                ('body', models.TextField(blank=True)),
+                ('articletype', models.IntegerField(default=0, choices=[(0, b'Article'), (1, b'Snippet')])),
+                ('headline', models.TextField(max_length=255, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Binary',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('date_modified', models.DateTimeField(auto_now=True, null=True)),
+                ('title', models.CharField(max_length=255, blank=True)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False)),
+                ('public', models.BooleanField(default=False)),
+                ('published', models.BooleanField(default=False)),
+                ('publish_from', models.DateTimeField(null=True, blank=True)),
+                ('publish_to', models.DateTimeField(null=True, blank=True)),
+                ('protected', models.BooleanField(default=False)),
+                ('body', models.TextField(blank=True)),
+                ('attachment', models.FileField(upload_to=b'')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ContentGroup',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('date_modified', models.DateTimeField(auto_now=True, null=True)),
+                ('title', models.CharField(max_length=255)),
+                ('parent', models.ManyToManyField(related_name='parent_rel_+', to='weblog.ContentGroup', blank=True)),
+                ('site', models.OneToOneField(to='sites.Site')),
+            ],
+            options={
+                'ordering': ('-date_created',),
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('date_modified', models.DateTimeField(auto_now=True, null=True)),
+                ('title', models.CharField(max_length=255, blank=True)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False)),
+                ('public', models.BooleanField(default=False)),
+                ('published', models.BooleanField(default=False)),
+                ('publish_from', models.DateTimeField(null=True, blank=True)),
+                ('publish_to', models.DateTimeField(null=True, blank=True)),
+                ('protected', models.BooleanField(default=False)),
+                ('body', models.TextField(blank=True)),
+                ('image_width', models.IntegerField(default=0)),
+                ('image_height', models.IntegerField(default=0)),
+                ('image', models.ImageField(height_field=models.IntegerField(default=0), width_field=models.IntegerField(default=0), upload_to=b'', blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Link',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('date_modified', models.DateTimeField(auto_now=True, null=True)),
+                ('title', models.CharField(max_length=255, blank=True)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False)),
+                ('public', models.BooleanField(default=False)),
+                ('published', models.BooleanField(default=False)),
+                ('publish_from', models.DateTimeField(null=True, blank=True)),
+                ('publish_to', models.DateTimeField(null=True, blank=True)),
+                ('protected', models.BooleanField(default=False)),
+                ('body', models.TextField(blank=True)),
+                ('external_link', models.CharField(max_length=3000, blank=True)),
+                ('original_url', models.CharField(max_length=3000, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SiteConfig',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('date_modified', models.DateTimeField(auto_now=True, null=True)),
+                ('template', models.CharField(max_length=255, blank=True)),
+                ('site', models.ForeignKey(to='sites.Site')),
+            ],
+            options={
+                'ordering': ('-date_created',),
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SiteUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('date_modified', models.DateTimeField(auto_now=True, null=True)),
+                ('website', models.CharField(max_length=255)),
+                ('user', models.OneToOneField(related_name='authuser', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ('-date_created',),
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('date_modified', models.DateTimeField(auto_now=True, null=True)),
+                ('tag', models.CharField(max_length=255)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False)),
+            ],
+            options={
+                'ordering': ('-date_created',),
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='link',
+            name='author',
+            field=models.ForeignKey(related_name='weblog_link_author', to='weblog.SiteUser'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='link',
+            name='sites',
+            field=models.ManyToManyField(to='sites.Site'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='link',
+            name='tags',
+            field=models.ManyToManyField(related_name='weblog_link_tags', to='weblog.Tag', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='image',
+            name='author',
+            field=models.ForeignKey(related_name='weblog_image_author', to='weblog.SiteUser'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='image',
+            name='sites',
+            field=models.ManyToManyField(to='sites.Site'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='image',
+            name='tags',
+            field=models.ManyToManyField(related_name='weblog_image_tags', to='weblog.Tag', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='binary',
+            name='author',
+            field=models.ForeignKey(related_name='weblog_binary_author', to='weblog.SiteUser'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='binary',
+            name='sites',
+            field=models.ManyToManyField(to='sites.Site'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='binary',
+            name='tags',
+            field=models.ManyToManyField(related_name='weblog_binary_tags', to='weblog.Tag', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='author',
+            field=models.ForeignKey(related_name='weblog_article_author', to='weblog.SiteUser'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='sites',
+            field=models.ManyToManyField(to='sites.Site'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='tags',
+            field=models.ManyToManyField(related_name='weblog_article_tags', to='weblog.Tag', blank=True),
+            preserve_default=True,
+        ),
+    ]
--- dammit/weblog/views.py
+++ dammit/weblog/views.py
@@ -5,34 +5,56 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 from datetime import date
+from django.contrib.sites.models import Site
 from .models import *
 
+
 def home(request):
-    articles = Article.objects.filter(publish_from__year=date.today().year, public=True)
+    """
+    Homepage
+    /
+    """
+    articles = Article.objects.filter(publish_from__year=date.today().year, public=True, sites__id=get_current_site(request).id)
     return render(request, settings.TEMPLATE_DIR + 'home_overview.html', {'articles': articles})
     #return HttpResponse('You are home. Have a hot beverage.')
 
 
 def article(request, article_id):
+    """
+    Article detail view
+    /p/<id>/
+    """
     article = get_object_or_404(Article, pk=article_id, sites__id=get_current_site(request).id)
     return render(request, settings.TEMPLATE_DIR + 'article.html', {'article': article})
 
 
 def article_archive(request, year=None):
+    """
+    Article archive
+    /archive/
+    """
     if year:
-        articles = Article.objects.filter(publish_from__year=str(year), public=True)
+        articles = Article.objects.filter(publish_from__year=str(year), public=True, sites__id=get_current_site(request).id)
     else:
-        articles = Article.objects.filter(publish_from__year=date.today().year, public=True)
+        articles = Article.objects.filter(publish_from__year=date.today().year, public=True, sites__id=get_current_site(request).id)
     return render(request, settings.TEMPLATE_DIR + 'archive.html', {'articles': articles})
     #return HttpResponse('You are viewing the archive (year: {0}).'.format(year))
 
 
 def link_archive(request, year=None):
-    articles = Link.objects.filter(publish_from__year=year, public=True)
+    """
+    Link (blogmark) archive
+    /m/
+    """
+    links = Link.objects.filter(publish_from__year=year, public=True, sites__id=get_current_site(request).id)
     return render(request, settings.TEMPLATE_DIR + 'link_archive.html', {'links': links})
     #return HttpResponse('You are viewing the link archive (year: {0}).'.format(year))
 
 
 def link(request, item_id):
-    link = get_object_or_404(link, pk=item_id)
+    """
+    Link (blogmark)
+    /m/<id>/
+    """
+    link = get_object_or_404(Link, pk=item_id, sites__id=get_current_site(request).id)
     return render(request, settings.TEMPLATE_DIR + 'link.html', {'link': link})
