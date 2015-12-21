a0e31727a0de4ab1c4b426af158333b1bc674aee
Michiel Scholten <michiel@diginaut.net>
2015-12-21 14:48:32 +0100

    Made wsgi.py work

--- kontent/kontentsite/wsgi.py
+++ kontent/kontentsite/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dammit.settings")
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kontentsite.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
