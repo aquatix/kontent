5398c96c825fee37754acc69ed405efff5d0ed29
Michiel Scholten <michiel@diginaut.net>
2016-08-21 15:45:03 +0200

    Updates, try to make running under Django 1.10

--- kontent/kontentsite/urls.py
+++ kontent/kontentsite/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'kontentsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+admin.autodiscover()
+
+urlpatterns = [
     url(r'^', include('kontent.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
